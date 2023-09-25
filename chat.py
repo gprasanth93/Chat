14392b6222ee21f9504256bf4c606fcb248e5220
gprasanth93 <gprasanth93@gmail.com>
2023-09-25 10:41:47 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,1 +1,22 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi_webtransport import WebTransport, WebTransportState
 
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def chat_page(request: WebTransport):
+    state = request.state
+
+    if state == WebTransportState.ESTABLISHED:
+        return templates.TemplateResponse("chat.html", {"request": request})
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
